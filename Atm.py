9b2c4e72031173512325a9879c4dd74c9a020b68
nallabh2 <82625441+nallabh2@users.noreply.github.com>
2022-04-11 17:47:28 -0500

    Update Atm.py

--- Atm.py
+++ Atm.py
@@ -1,7 +1,12 @@
 class Atm:
-  __init__(self,cardNum,pinNum):
+  __init__(self,cardNum,pinNum,initialBalance):
     self.cardNum=cardNum
     self.pinNum=pinNum
+    self.balance=initialBalance
     
-  def CashWithdrawl(card,pin):
-  	
+  def CashWithdrawl(self,amount,card,pin):
+  	print(amount+" has been withdrawn")
+    self.balance-=amount
+    
+  def BalanceEnquiry(self,card,pin):
+    print("your balance is "+self.balance)
